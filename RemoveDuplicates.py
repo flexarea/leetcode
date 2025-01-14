58b173119ce932882bee9e8a6ab4790dddf3ac07
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2025-01-14 09:27:28 -0500

    passing initial test cases

--- RemoveDuplicates.py
+++ RemoveDuplicates.py
@@ -3,20 +3,25 @@ def removeDuplicates(nums):
         return nums
     p1 = 0
     p2 = 0
+    size = len(nums) - 1
     n = 0
+    k = 0
 
-    while n < len(nums)-1:
+    while n < size:
         if p1 == p2:
             p2 += 1
         else:
             if nums[p1] == nums[p2]:
                 nums.remove(nums[p1])
                 n += 1
+                k += 1
             else:
+                if p2 == len(nums) - 1:
+                    k += 1
                 p1 += 1
                 p2 += 1
                 n += 1
-    return nums
+    return k
 
 
-print(removeDuplicates([0, 0, 1, 1, 2, 2, 3, 3, 4]))
+print(type(removeDuplicates([0, 0, 1, 1, 2, 2, 3, 3, 4])))
