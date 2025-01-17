08371abb9673a9497d4a680b25be710ef5563457
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2025-01-17 16:11:30 -0500

    still trying

--- RemoveDuplicateSortedArray.py
+++ RemoveDuplicateSortedArray.py
@@ -2,43 +2,34 @@ def removeDuplicates(nums):
     left_pointer = 0
     prev = 0
     k = 0
+    identical = True
     for i in range(len(nums)-1):
-        identical = True
-        prev = i
+        c0 = 1
         for j in range(len(nums) - 2):
-            c = 0
             rep = 0
             if nums[j+1] == nums[prev] and identical:
-                if c < 3:
-                    c += 1
-                else:
-                    left_pointer = j+1
-                    prev = left_pointer
+                if c0 < 3:
+                    c0 += 1
                     k += 1
+                else:
                     identical = False
             else:
                 if nums[j+1] != nums[prev]:
                     if rep:  # check if we are already checking for a specific number
                         # check if number we currently at is equal to number we are looking for
                         if nums[j+1] == rep:
-                            if c < 2:
-                                c += 1
-                                # replace value pointed by left_pointer
-                                nums[left_pointer] = nums[j+1]
-                                left_pointer += 1
+                            if c0 < 3:
+                                nums[i] = nums[rep]
+                                c0 += 1
                                 k += 1
                             else:
-                                left_pointer = j+1
-                                k += 1
+                                identical = False
                                 prev = left_pointer
                         else:
                             # restart on new number
-                            rep = 0
-                            c = 0
+                            k += 1
                     else:
-                        rep = nums[j+1]
-                        nums[left_pointer] = nums[j+1]
-                        left_pointer += 1
+                        rep = j+1
                         c += 1
 
 
