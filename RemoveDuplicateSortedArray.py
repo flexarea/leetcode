1908196d242755ad6fa1b82d4fc41c0ab974c8e5
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2025-01-17 16:47:10 -0500

    found solution that makes me want to shoot myself

--- RemoveDuplicateSortedArray.py
+++ RemoveDuplicateSortedArray.py
@@ -1,31 +1,16 @@
 def removeDuplicates(nums):
-    left_pointer = 0
-    prev = 0
-    k = 0
-    identical = True
-    c0 = 1
-    for i in range(len(nums)):
-        if nums[i] == nums[prev]:
-            if c0 < 3:
-                c0 += 1
-                k += 1
-                break
-        for j in range(i+1, len(nums)):
-            rep = 0
-            if nums[j+1] != nums[prev]:
-                if rep:  # check if we are already checking for a specific number
-                    if nums[j+1] == rep:
-                        if c0 < 3:
-                            nums[i] = nums[rep]
-                            c0 += 1
-                            k += 1
-                        else:
-                            prev = left_pointer
-                    else:
-                        k += 1
-                else:
-                    rep = j+1
-                    c += 1
+    if len(nums) <= 2:
+        return len(nums), nums
+
+    # index where to place the next number
+    p = 2
+
+    for i in range(2, len(nums)):
+        if nums[i] != nums[p-2]:
+            nums[p] = nums[i]
+            p += 1
+
+    return p, nums
 
 
 print(removeDuplicates([0, 0, 1, 1, 1, 1, 2, 3, 3]))
