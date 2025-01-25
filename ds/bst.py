008afc9d10cf4473241dc9908afcb9222d4de155
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2025-01-25 13:52:40 -0500

    added insertNode

--- ds/bst.py
+++ ds/bst.py
@@ -2,25 +2,41 @@ class Tree:
     def __init__(self, val=None):
         self.value = val
 
-        if self.value:
+        if self.value is not None:
             self.left = Tree()
             self.right = Tree()
         else:
             self.left = None
             self.right = None
 
-    def isEmpty(self, val):
-        return (self.value == None)
+    def isEmpty(self):
+        return self.value is None
 
     def insertNode(self, val):
-        if self.isEmpty(self.value):
+        if self.isEmpty():
             self.value = val
         elif val < self.value:
-            self.left.insertNode(val)
+            if self.left.value is None:
+                self.left = Tree(val)
+            else:
+                self.left.insertNode(val)
         elif val > self.value:
-            self.right.insertNode(val)
+            if self.right.value is None:
+                self.right = Tree(val)
+            else:
+                self.right.insertNode(val)
         else:
             return
+
+    def findNode(self, val):
+        if self.isEmpty():
+            return f"Value not found"
+        if val > self.value:
+            return self.right.findNode(val)
+        elif val < self.value:
+            return self.left.findNode(val)
+        else:
+            return f"Found={self.value}"
 
     def __str__(self):
         if self.value == None:
@@ -30,4 +46,5 @@ class Tree:
 
 x = Tree(10)
 x.insertNode(4)
-print(x)
+x.insertNode(12)
+print(x.findNode(4))
