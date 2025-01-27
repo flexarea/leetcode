8a1208823b9b1029b0f45d8429d1b0c243c49b63
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2025-01-27 09:45:11 -0500

    okay. I can create a bst from scratch now (to some extent)

--- ds/binary_tree.py
+++ ds/binary_tree.py
@@ -6,8 +6,8 @@ class Tree:
             self.left = Tree()
             self.right = Tree()
         else:
-            self.left = None
             self.right = None
+            self.left = None
 
     def isEmpty(self):
         return self.value is None
@@ -28,34 +28,20 @@ class Tree:
         else:
             return
 
-    def findNode(self, val):
-        if self.isEmpty():
-            return f"Value not found"
-        if val > self.value:
-            return self.right.findNode(val)
-        elif val < self.value:
-            return self.left.findNode(val)
-        else:
-            return f"Found={self.value}"
-
     def traversal(self):
         if self.isEmpty():
             return
-
         self.left.traversal()
         print(self.value)
         self.right.traversal()
 
     def __str__(self):
-        if self.value == None:
+        if self.value is None:
             return f"None"
         return f"root={self.value} left={self.left.value} right={self.right.value}"
 
 
 x = Tree(10)
-x.insertNode(4)
 x.insertNode(12)
-# print(x.findNode(4))
-
-# ---------------- Traversal
+x.insertNode(4)
 x.traversal()
