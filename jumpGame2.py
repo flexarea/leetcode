fe54dff4a56dd02503d8d367889baef0ad1cf7f3
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2025-07-17 08:32:21 -0400

    jfdkkfd

---
+++ jumpGame2.py
@@ -0,0 +1,16 @@
+def jump(nums):
+    all_jumps = []
+    step = 0
+    target = len(nums)-1
+    min_jump = -1
+    recorder = 0
+    for i in range(len(nums)):
+        step += 1
+        if (nums[i] > 1 and j < target){
+            if (i + nums[j] >= target){
+                step += 1
+                all_jumps.append(step)
+                recorder = 0
+                continue
+            }
+        }
