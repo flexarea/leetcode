47ef6f799440fa548eb51df1f5ae7707f1416e5e
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2025-07-17 11:13:32 -0400

    looked it up

--- jumpGame2.py
+++ jumpGame2.py
@@ -1,16 +1,15 @@
 def jump(nums):
-    all_jumps = []
-    step = 0
-    target = len(nums)-1
-    min_jump = -1
-    recorder = 0
-    for i in range(len(nums)):
-        step += 1
-        if (nums[i] > 1 and j < target){
-            if (i + nums[j] >= target){
-                step += 1
-                all_jumps.append(step)
-                recorder = 0
-                continue
-            }
-        }
+    jumps = 0
+    max_jump = 0
+    curr_end = 0
+    for i in range(len(nums) - 1):
+        max_jump = max(max_jump, i + nums[i])
+
+        if i == curr_end:
+            jumps += 1
+            curr_end = max_jump
+
+            if curr_end >= len(nums) - 1:
+                break
+
+    return jumps
