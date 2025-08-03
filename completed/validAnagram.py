41eaf96f9d28cefe89c71d296f6f91feaf0104c1
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2025-08-03 14:56:27 -0400

    comment

--- completed/validAnagram.py
+++ completed/validAnagram.py
@@ -4,10 +4,12 @@ def isAnagram(s, t):
 
     count = {}
     for char_s, char_t in zip(s, t):
+        # increment value in s and decrement in t in the same dictionary (best space complexity)
         count[char_s] = count.get(char_s, 0) + 1
         count[char_t] = count.get(char_t, 0) - 1
 
     for var in count.values():
+        # since incrementing and decrementing cancel out each character should have the value 0 if they are anagrams
         if var != 0:
             return False
 
