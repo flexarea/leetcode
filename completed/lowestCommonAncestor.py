6974a760d1bfb93e6cc1050e96855824baa6bf13
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2025-10-03 14:34:51 -0400

    changed implementation

--- completed/lowestCommonAncestor.py
+++ completed/lowestCommonAncestor.py
@@ -1,22 +1,10 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if root is None:
-            return
-
-        if root == p or root == q:
-            return root
-
-        left = self.lowestCommonAncestor(root.left, p,q)
-        right = self.lowestCommonAncestor(root.right, p,q)
-
-        if left and right:
-            return root
-
-        return left if left else right
+        cur = root
+        while cur:
+            if p.val < cur.val and q.val < cur.val:
+                cur = cur.left
+            elif p.val > cur.val and q.val > cur.val:
+                cur = cur.right
+            else:
+                return cur
