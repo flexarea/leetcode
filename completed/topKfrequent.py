3ab8a63790950afee74d767042fefd0eb66f489a
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2025-10-21 16:36:22 -0400

    resolved

--- completed/topKfrequent.py
+++ completed/topKfrequent.py
@@ -12,3 +12,13 @@ def topKFrequent(nums, k):
 
 
 print(topKFrequent([1, 1, 1, 2, 2, 3], 2))
+
+
+# progress?
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        from collections import Counter
+        dic = Counter(nums)
+        sorted_dic = dict(
+            sorted(dic.items(), key=lambda item: item[1], reverse=True)).keys()
+        return list(sorted_dic)[:k]
