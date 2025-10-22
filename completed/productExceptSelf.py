c55fd0adf411b5c441efdd9a773b45340027e38a
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2025-10-22 09:49:08 -0400

    changed solution

--- completed/productExceptSelf.py
+++ completed/productExceptSelf.py
@@ -1,25 +1,15 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * (len(nums))
+        prefix = 1
 
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
 
-def productExceptSelf(nums):
-    answer = [None]*len(nums)
-    left_prod = 1
-    # left traversal
-    for i in range(len(nums)):
-        if i == 0:
-            answer[i] = 1
-        else:
-            left_prod *= nums[i-1]
-            answer[i] = left_prod
+        postfix = 1
 
-    # right traversal
-    right_prod = 1
-    for i in range(len(nums)-1, -1, -1):
-        answer[i] *= right_prod
-        right_prod *= nums[i]
-
-    return answer
-
-
-nums = [1, 2, 3, 4]
-# expected = Output: [24,12,8,6]
-print(productExceptSelf(nums))
+        for i in range(len(nums)-1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
