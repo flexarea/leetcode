f89bd1c9589cee20817c37dfc38dcc84c5fe7e57
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2025-10-26 14:18:55 -0400

    my solution work

--- validSudoku.py
+++ validSudoku.py
@@ -11,10 +11,11 @@ class Solution:
             seen = set()  # check for duplicate in every grid
             for r in range(r_bound, ROW):
                 for c in range(c_bound, COL):
-                    if board[r][c] in seen:
+                    if board[r][c] != "." and board[r][c] in seen:
                         return False  # right? -> could be a tiny optimization as we stop there
                     res[i].append(board[r][c])
-                    seen.add(board[r][c])
+                    if board[r][c] != ".":
+                        seen.add(board[r][c])
             if COL == len(board):
                 ROW += 3
                 r_bound += 3
@@ -26,10 +27,41 @@ class Solution:
                 c_bound += 3
                 COL += 3
             i += 1
-        # check for rows
+
+        # check for valid rows
+        ROW, COL = len(board), len(board[0])
+
+        for r in range(ROW):
+            row_set = set()  # create a new set to check the rows
+            for c in range(COL):
+                if board[r][c] != "." and board[r][c] in row_set:
+                    return False
+                if board[r][c] != ".":
+                    row_set.add(board[r][c])
+
+        # check for valid columns
+
+        for r in range(ROW):
+            col_set = set()
+            for c in range(COL):
+                if board[c][r] != "." and board[c][r] in col_set:
+                    return False
+                if board[c][r] != ".":
+                    col_set.add(board[c][r])
+        return True
 
 
-board = [["5", "3", ".", ".", "7", ".", ".", ".", "."],
+board1 = [["5", "3", ".", ".", "7", ".", ".", ".", "."],
+          ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+          [".", "9", "8", ".", ".", ".", ".", "6", "."],
+          ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+          ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+          ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+          [".", "6", ".", ".", ".", ".", "2", "8", "."],
+          [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+          [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
+
+board = [["8", "3", ".", ".", "7", ".", ".", ".", "."],
          ["6", ".", ".", "1", "9", "5", ".", ".", "."],
          [".", "9", "8", ".", ".", ".", ".", "6", "."],
          ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
@@ -40,4 +72,4 @@ board = [["5", "3", ".", ".", "7", ".", ".", ".", "."],
          [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
 
 new_s = Solution()
-new_s.isValidSudoku(board)
+print(new_s.isValidSudoku(board))
