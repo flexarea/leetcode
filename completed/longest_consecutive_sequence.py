f0dfb50a8674ef82eb8debd8657f8d2e5cbb3bd6
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2025-10-28 11:12:52 -0400

    new sol

--- completed/longest_consecutive_sequence.py
+++ completed/longest_consecutive_sequence.py
@@ -16,3 +16,24 @@ class Solution:
                 res = max(res, curr)
                 curr = 1
         return max(res, curr)
+
+# O(n) complexity
+
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        sorted_nums = sorted(nums)
+
+        res, count = 0, 1  # left pointer
+
+        for i in range(1, len(sorted_nums)):
+            if sorted_nums[i-1] == sorted_nums[i] - 1:
+                count += 1
+            else:
+                res = max(res, count)
+                count = 1
+        res = max(res, count)
+        return res
