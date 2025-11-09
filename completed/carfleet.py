70c35b64be67ad11ddbb141833cd8a03274e5ecf
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2025-11-09 12:39:17 -0500

    changed sol

--- completed/carfleet.py
+++ completed/carfleet.py
@@ -1,10 +1,12 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        car_arr = [[pos, spe] for pos, spe in zip(position, speed)]
-        stack= []
+        #   we want to combine position and speed into a single iterable of pairs
+        #   we want to sort the iterable in ascending order then loop in backward
+        pairs = sorted([(p, s) for p, s in zip(position, speed)])  # nlogn
+        stack = []
 
-        for pos, spe in sorted(car_arr)[::-1]:
-            stack.append((target- pos) / spe)
+        for p, s in pairs[::-1]:
+            stack.append((target-p)/s)
             if len(stack) >= 2 and stack[-1] <= stack[-2]:
                 stack.pop()
         return len(stack)
