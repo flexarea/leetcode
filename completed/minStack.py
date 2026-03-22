75f76b1b40731d7506fe37772c1aa86d42205446
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2026-03-22 12:43:33 -0400

    changed

--- completed/minStack.py
+++ completed/minStack.py
@@ -25,4 +25,29 @@ class MinStack:
 # obj.push(val)
 # obj.pop()
 # param_3 = obj.top()
-# param_4 = obj.getMin()
+
+# tuple implementation
+"""
+class MinStack:
+
+    def __init__(self):
+        self.min = float("inf")
+        self.stack = []
+
+    def push(self, val: int) -> None:
+
+        curr_min = val if not self.stack else min(val, self.stack[-1][1])
+        self.stack.append((val, curr_min))
+        
+
+    def pop(self) -> None:
+        self.stack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+        
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
+"""
