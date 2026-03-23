1fe071b3a481b08feeb62d1f14879e7771547cb4
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2026-03-23 18:39:17 -0400

    was wrong

--- completed/3sum.py
+++ completed/3sum.py
@@ -21,32 +21,3 @@ class Solution:
                     while nums[l] == nums[l-1] and l < r:
                         l += 1
         return res
-
-# More optimal (28ms)
-
-
-class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        sorted_nums = sorted(nums)
-        res = []
-        total = 0
-        for i in range(len(sorted_nums)):
-            left, right = i+1, len(sorted_nums)-1
-            while left < right:
-                curr_triplet_sum = sorted_nums[left] + \
-                    sorted_nums[right] + sorted_nums[i]
-                if 0 == curr_triplet_sum:
-                    triplet = [sorted_nums[i],
-                               sorted_nums[left], sorted_nums[right]]
-                    if not triplet in res:
-                        res.append(triplet)
-                        left += 1
-                        right -= 1
-                    else:
-                        left += 1
-                        right -= 1
-                elif curr_triplet_sum < 0:
-                    left += 1
-                else:
-                    right -= 1
-        return res
