26adbbbe8a362e8bf9116e0585d4b6273ab69b74
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2026-03-23 17:26:29 -0400

    fixed sol

--- completed/twoSumII.py
+++ completed/twoSumII.py
@@ -1,10 +1,15 @@
+# Two pointer optimal solution
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        num_dic = {}
-
-        for index, num in enumerate(numbers):
-            if target - num in num_dic:
-                return sorted([index+1, num_dic[target-num]])
+        """
+        [2,3,4]
+        """
+        left, right = 0, len(numbers) - 1
+        while left < right:
+            if numbers[left] + numbers[right] == target:
+                return [left+1, right+1]
+            if numbers[left] + numbers[right] > target:
+                right -= 1
             else:
-                num_dic[num] = index + 1
+                left += 1
         return []
