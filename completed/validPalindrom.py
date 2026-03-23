d2c785ccfd9901d0807bac02cb1cb0ebbf3528aa
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2026-03-23 16:49:55 -0400

    new sol

--- completed/validPalindrom.py
+++ completed/validPalindrom.py
@@ -1,4 +1,22 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        res = ''.join(ch.lower() for ch in s if ch.isalnum())
-        return res == res[::-1]
+        import string
+        if not s:
+            return True
+        clean_str = ""
+
+        # first arrange string
+        for char in s:
+            if not char.isalnum():
+                continue
+            clean_str += char.lower()
+
+        left, right = 0, len(clean_str)-1
+
+        while left <= right:
+            if clean_str[left] != clean_str[right]:
+                return False
+            left += 1
+            right -= 1
+
+        return True
