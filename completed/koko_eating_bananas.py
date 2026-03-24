f37b72ae1866c446fd382219483d916fb3c1cc2c
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2026-03-24 14:35:26 -0400

    koko

--- completed/koko_eating_bananas.py
+++ completed/koko_eating_bananas.py
@@ -1,22 +1,15 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        import math
-
-        low = 1
-        high = max(piles)
-        k = 0
-        curr_res = math.inf
+        low, high = 1, max(piles)
+        total = sum(piles)
 
         while low <= high:
-            k = low + (high - low) // 2
-            total_h = 0
-            for i in piles:
-                total_h += math.ceil(i/k)
-            if total_h <= h:
-                curr_res = min(curr_res, k)
-            if total_h > h:
+            k = (low + high) // 2
+            k_sum = 0
+            for p in piles:
+                k_sum += math.ceil(p/k)
+            if k_sum > h:
                 low = k + 1
             else:
                 high = k - 1
-
-        return curr_res
+        return low
