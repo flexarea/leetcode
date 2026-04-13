c607f29ecc34aefdce5de9a12417da79deaa02b3
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2026-04-12 23:31:01 -0400

    solved

--- ds/test.py
+++ ds/test.py
@@ -1,29 +1,64 @@
-from collections import defaultdict, deque
+from collections import deque
 
-n = 8
-a = [(0, 1), (0, 3), (1, 2),
-     (3, 4), (3, 7), (3, 6), (4, 2), (4, 5), (5, 2)]
-
-# matrix
-m = [[0]*n for _ in range(n)]
-
-for u, v in a:
-    m[u][v] = 1
-
-# 1. Depth First Search
+# [20, 21, 23, 26, 28]
+# now = 40
 
 
-def dfs(matrix):
-    seen = set()
-    seen.add(0)
+class SessionWindow:
+    def __init__(self):
+        self._timestamps = deque()
 
-    stack = [0]
+    def evict_old(self, now: float, window: float):
+        cutoff = now - window
+        while self._timestamps and self._timestamps[0] < cutoff:
+            self._timestamps.popleft()
 
-    while stack:
-        curr = stack.pop()
-        print(curr)
-        for neighbor in range(n):
-            if m[curr][neighbor]:
-                if neighbor not in seen:
-                    stack.append(neighbor)
-                    seen.add(neighbor)
+    def add(self, timestamp: float):
+        self._timestamps.append(timestamp)
+
+    def count(self) -> int:
+        return len(self._timestamps)
+
+
+store = {}  # apikey -> session  object
+window = 60
+
+
+def Allow(api_key: str, now: float) -> bool:
+
+    # check for new api_key
+    if api_key not in store:
+        store[api_key] = SessionWindow()
+        return True
+
+    # evict
+    store[api_key].evict_old(now, window)
+
+    # check rate limit
+    if store[api_key].count() >= 10:
+        return False
+
+    store[api_key].add(now)
+    return True
+
+
+seen_request = set()
+timestamps = deque()
+window = 30
+
+
+def is_duplicate(request_id: str, now: float) -> bool:
+
+    # evict old entries - worst case O(n)
+    while timestamps and timestamps[0][0] < (now - window):
+        _, old_id = timestamps.popleft()
+        seen_request.discard(old_id)
+
+    # duplicate check O(1)
+    if request_id in seen_request:
+        return True
+
+    # check if it's a new request
+    seen_request.add(request_id)
+    timestamps.append((now, request_id))
+    return False
