0e6cb515f0f4a51b8c811c6b01c1818ee8bd0f64
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2026-06-25 15:43:11 -0400

    revised sol

--- completed/productExceptSelf.py
+++ completed/productExceptSelf.py
@@ -3,13 +3,25 @@ class Solution:
         res = [1] * (len(nums))
         prefix = 1
 
+        # Prefix product at an index is the product of all element on its left
+        # res[0] should be 1  because there is no product on the left of res[0]
         for i in range(len(nums)):
             res[i] = prefix
             prefix *= nums[i]
 
+        # We do the same thing for sufix product (product of all element on the right)
         postfix = 1
 
         for i in range(len(nums)-1, -1, -1):
+            """
+            the block below is equivalent to having a result2 array for postfix products of nums and doing:
+            result2[i] = posfix
+            postfix *= nums[i]
+
+            But because we will end up multiplying result with result2, we can just directly multiply every postfix with the 
+            same element at the index it would be multiplied with
+
+            """
             res[i] *= postfix
             postfix *= nums[i]
         return res
