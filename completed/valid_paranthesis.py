5fd2eb46e04fa04a1279ea3dbaa1b6810d19ebb4
Esdras Ntuyenabo <esdrasntuyenabo40@gmail.com>
2026-07-10 23:13:46 -0700

    new question-ish

---
+++ completed/valid_paranthesis.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dic = {'}': '{', ')': '(', ']': '['}
+        stack = []
+
+        for i in s:
+            if i in dic:
+                if not stack or stack.pop() != dic[i]:
+                    return False
+            else:
+                stack.append(i)
+        return not stack
